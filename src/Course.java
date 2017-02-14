c8cd28bb710e0c3d484db8908b14242b1bd7b4e6
WeijaZhou <wzhou94@uwo.ca>
2017-02-13 19:12:16 -0500

    Add Course class

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by WeijaZhou on 2017-02-13.
+ */
+public class Course
+{
+    private String name;
+    private String description;
+    public Course(String cname, String description){
+        this.name = cname;
+        this.description = description;
+
+    }
+}
